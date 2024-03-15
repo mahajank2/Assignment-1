5456777eb293ca00e2e0bace3e4abf86faeca9eb
mahajank2 <156012875+mahajank2@users.noreply.github.com>
2024-03-14 19:11:03 -0600

    Add files via upload

---
+++ C#/Program.cs
@@ -0,0 +1,17 @@
+﻿using Assignment_1;
+
+class Program
+{
+    static void Main()
+    {
+        var student1 = StudentMethods.CreateStudent(1, StudentType.FullTime, "Monika", "Gupta");
+        var student2 = StudentMethods.CreateStudent(2, StudentType.FullTime, "Anjali", "Mahajan");
+        var student3 = StudentMethods.CreateStudent(3, StudentType.PartTime, "Shivam", "Sharma");
+
+        Console.WriteLine("Assignment One");
+
+        StudentMethods.OutputStudent(student1);
+        StudentMethods.OutputStudent(student2);
+        StudentMethods.OutputStudent(student3);
+    }
+}
